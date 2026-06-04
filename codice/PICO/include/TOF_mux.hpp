ceeedcbfab57411766df7484dcaac0c69c72de01
antospa <37903976+antospa@users.noreply.github.com>
2026-06-04 12:07:44 +0200

    Update TOF_mux.hpp

--- codice/PICO/include/TOF_mux.hpp
+++ codice/PICO/include/TOF_mux.hpp
@@ -59,6 +59,57 @@ inline void initTOF(SparkFun_VL53L5CX sensor[TOF_SENSOR_COUNT], TwoWire &i2c_bus
 
 // ------------------------------------------------------------------------------ //
 
+// Round-robin: legge UN solo sensore. Preserva l'ultimo valore valido se !isDataReady.
+inline void readTOFSingle(
+    SparkFun_VL53L5CX sensor[TOF_SENSOR_COUNT],
+    TwoWire &i2c_bus,
+    const bool tof_ready[TOF_SENSOR_COUNT],
+    int16_t TOF_1D[TOF_SENSOR_COUNT][TOF_COLUMN_COUNT],
+    int i
+) {
+    if (!tof_ready[i]) {
+        return;
+    }
+
+    i2c_bus.beginTransmission(0x70);
+    i2c_bus.write(1 << i);
+    i2c_bus.endTransmission();
+
+    if (!sensor[i].isDataReady()) {
+        return; // keep last valid reading
+    }
+
+    VL53L5CX_ResultsData data;
+    sensor[i].getRangingData(&data);
+
+    int16_t distances[TOF_RESOLUTION];
+
+    for (int cell = 0; cell < TOF_RESOLUTION; cell++) {
+        int original_row = cell / TOF_GRID_SIZE;
+        int original_col = cell % TOF_GRID_SIZE;
+        int rotated_index = ((TOF_GRID_SIZE - 1 - original_col) * TOF_GRID_SIZE) + original_row;
+        distances[rotated_index] = data.distance_mm[cell];
+    }
+
+#if TOF_DEBUG_PRINT_MATRIX
+    printTOFMatrix(i, distances);
+#endif
+
+    for (int col = 0; col < TOF_COLUMN_COUNT; col++) {
+        int16_t minimum = distances[col];
+
+        for (int row = 1; row < TOF_GRID_SIZE; row++) {
+            int16_t current_value = distances[(row * TOF_GRID_SIZE) + col];
+            if (current_value < minimum) {
+                minimum = current_value;
+            }
+        }
+
+        TOF_1D[i][TOF_COLUMN_COUNT - 1 - col] = minimum;
+    }
+}
+
+// Wrapper bloccante (tutti e 8 i sensori). Evita nel loop principale.
 inline void readTOF(
     SparkFun_VL53L5CX sensor[TOF_SENSOR_COUNT],
     TwoWire &i2c_bus,
@@ -66,50 +117,7 @@ inline void readTOF(
     int16_t TOF_1D[TOF_SENSOR_COUNT][TOF_COLUMN_COUNT]
 ) {
     for (int i = 0; i < TOF_SENSOR_COUNT; i++) {
-        int16_t distances[TOF_RESOLUTION];
-
-        for (int cell = 0; cell < TOF_RESOLUTION; cell++) {
-            distances[cell] = 4000;
-        }
-
-        if (!tof_ready[i]) {
-            continue;
-        }
-
-        i2c_bus.beginTransmission(0x70);
-        i2c_bus.write(1 << i);
-        i2c_bus.endTransmission();
-
-        if (sensor[i].isDataReady()) {
-            VL53L5CX_ResultsData data;
-            sensor[i].getRangingData(&data);
-
-            for (int cell = 0; cell < TOF_RESOLUTION; cell++) {
-                int original_row = cell / TOF_GRID_SIZE;
-                int original_col = cell % TOF_GRID_SIZE;
-                int rotated_index = ((TOF_GRID_SIZE - 1 - original_col) * TOF_GRID_SIZE) + original_row;
-
-                distances[rotated_index] = data.distance_mm[cell];
-            }
-        }
-
-#if TOF_DEBUG_PRINT_MATRIX
-        printTOFMatrix(i, distances);
-#endif
-
-        for (int col = 0; col < TOF_COLUMN_COUNT; col++) {
-            int16_t minimum = distances[col];
-
-            for (int row = 1; row < TOF_GRID_SIZE; row++) {
-                int16_t current_value = distances[(row * TOF_GRID_SIZE) + col];
-
-                if (current_value < minimum) {
-                    minimum = current_value;
-                }
-            }
-
-            TOF_1D[i][TOF_COLUMN_COUNT - 1 - col] = minimum;
-        }
+        readTOFSingle(sensor, i2c_bus, tof_ready, TOF_1D, i);
     }
 }
 
